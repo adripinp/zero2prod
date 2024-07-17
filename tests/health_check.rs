296d0e0e6a301367b4bf8d002ebd501ae852e1f0
vboxuser <vboxuser@UbuntuAdri.myguest.virtualbox.org>
2024-07-17 10:37:32 -0500

    Implementing Our Test 2_with result

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,21 +1,28 @@
 //use zero2prod::main;
 
 #[tokio::test]
-
 async fn health_check_works() {
-    spawn_app().await.expect("Failed to spawn our app.");
-    let client = reqwest::Client::new();
-
-    let response = client
-        .get("http://127.0.0.1:8080/health")
-        .send()
-        .await
-        .expect("Failed to send request.");
-        
-    assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
+    spawn_app()
 }
 
-async fn spawn_app() -> Result <(), std::io::Error> {
-    zero2prod::run().await
+//async fn health_check_works() {
+//    spawn_app().await.expect("Failed to spawn our app.");
+//    let client = reqwest::Client::new();
+
+//    let response = client
+//        .get("http://127.0.0.1:8080/health")
+//        .send()
+//        .await
+//        .expect("Failed to send request.");
+        
+//    assert!(response.status().is_success());
+//    assert_eq!(Some(0), response.content_length());
+
+//}
+
+
+fn spawn_app() {
+    let server= zero2prod::run().expect("Failed to bind asddress");
+
+    let _ = tokio::spawn(server);
 }
