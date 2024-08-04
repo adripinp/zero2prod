bdd7581a75544b2a89b365eb6f1f927524011bba
vboxuser <vboxuser@UbuntuAdri.myguest.virtualbox.org>
2024-08-04 12:25:08 -0500

    Test_Responde 200

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,28 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run(listener:TcpListener) -> Result<Server, std::io::Error> {
+//pub fn run(listener:TcpListener) -> Result<Server, std::io::Error> {
+//    let server = HttpServer::new(|| {
+//        App::new()
+//            .route("/health_check", web::get().to(health_check))
+//    })
+//    .listen(listener)?
+//    .run();
+    // No .await here
+//    Ok (server)
+//}
+
+// Let's start simple: we always return a 200 ok
+
+async fn subscribe()-> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener:TcpListener)->Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
